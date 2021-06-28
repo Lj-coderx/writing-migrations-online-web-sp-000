69a4f44c06bc0aaae7f672493e8946df5349df42
Lj <tnedutsl457892@gmail.com>
2021-06-28 05:54:27 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,10 +1,6 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     create_table :students do |t|
-      t.string :first_name
-      t.string :last_name
-      t.integer :grade
-      t.string :birthdate
       t.string :name
     end
   end
